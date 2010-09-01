ff6cdd7220ec3faa292549b718d4b42a048f6010
Otávio de Freitas e Castro Sampaio <osampaio@riopro.com.br>
2010-09-01 15:05:48 -0300

    testes para has_deja_vue

--- spec/deja_vue/has_deja_vue_spec.rb
+++ spec/deja_vue/has_deja_vue_spec.rb
@@ -3,12 +3,10 @@ require 'spec_helper'
 describe DejaVue::InstanceMethods do
   before(:each) do
     clear_dbs
+	  @supplier = Supplier.new(:name => 'a supplier')
+		@product = Product.new(:title => 'The Product', :description => 'The Product is the new kid on the block', :dimensions => '12x12')
 	end
 	describe "version_changes" do
-		before(:each) do
-			@supplier = Supplier.new(:name => 'a supplier')
-			@product = Product.new(:title => 'The Product', :description => 'The Product is the new kid on the block', :dimensions => '12x12')
-		end
 		describe "after save" do
 			before(:each) do
 				@supplier.save
@@ -18,9 +16,10 @@ describe DejaVue::InstanceMethods do
 				@product.version_changes.should == ["dimensions", "title", "description"]
 				@supplier.version_changes.should == ['name']
 			end
-			it "should ignore fields setted to be ignored" do
-			end
-			it "should be empty if object has no changes" do
+			it "should keep track on update to" do	
+				@product.title = "TTT Prod"
+				@product.save!
+				@product.version_changes.should == ["title"]
 			end
 		end
 		describe "before save" do
@@ -28,6 +27,14 @@ describe DejaVue::InstanceMethods do
 				@product.version_changes.should be_nil
 				@supplier.version_changes.should be_nil
 			end
+		end
+	end
+	describe "histories(extra_options={})" do
+		it "should return an empty array for new record" do
+			@product.should be_new_record
+			@product.histories.should be_empty
+			@supplier.should be_new_record
+			@supplier.histories.should be_empty
 		end
 	end
 end
